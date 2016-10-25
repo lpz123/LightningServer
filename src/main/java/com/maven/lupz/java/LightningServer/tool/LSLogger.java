10802a1fa5338af8c4d05c8a5a52f56c34b878d4
lpz123 <25454190@qq.com>
2016-10-25 19:24:03 +0800

    +++修改了LSLogger的日志完整性 +++Mysql引擎版本从6.0.3降到5.1.38

--- src/main/java/com/maven/lupz/java/LightningServer/tool/LSLogger.java
+++ src/main/java/com/maven/lupz/java/LightningServer/tool/LSLogger.java
@@ -5,12 +5,12 @@ import org.apache.log4j.Logger;
 public class LSLogger {
 	private static Logger log=Logger.getLogger(LSLogger.class);
 	
-	public static void infoLogger(Class clazz,String message){
-		log.info(clazz.getName()+":"+message);
+	public static void infoLogger(Class<?> clazz,Object objx){
+		log.info(clazz.getName()+":"+objx);
 	}
 	
-	public static void errorLogger(Class clazz,String message){
-		log.error(clazz.getName()+":"+message);
+	public static void errorLogger(Class<?> clazz,Object objx){
+		log.error(clazz.getName()+":"+objx);
 	}
 	
 }
