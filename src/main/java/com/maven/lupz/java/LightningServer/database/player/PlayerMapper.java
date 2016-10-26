3db0373fb3fdf90497fcc521e6a7c9602b384718
lpz123 <25454190@qq.com>
2016-10-26 14:15:15 +0800

    --删除不必要的文件

--- src/main/java/com/maven/lupz/java/LightningServer/database/player/PlayerMapper.java
+++
@@ -1,7 +0,0 @@
-package com.maven.lupz.java.LightningServer.database.player;
-
-public interface PlayerMapper {
-	public void insertPlayerBean(PlayerBean bean);
-	public PlayerBean getPlayerBean(long roleId);
-	public void updatePlayerBean(PlayerBean bean);
-}
